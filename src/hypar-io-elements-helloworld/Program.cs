ca789fa42af816bfa60a2aeda6d6b0f74f8df83b
bertt <btemme@hotmail.com>
2020-10-07 12:04:02 +0200

    initial

---
+++ src/hypar-io-elements-helloworld/Program.cs
@@ -0,0 +1,17 @@
+﻿using Elements.Serialization.IFC;
+using Elements.Serialization.glTF;
+using System;
+
+namespace hypar_io_elements_helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var newModel = IFCModelExtensions.FromIFC("AC-20-Smiley-West-10-Bldg.ifc");
+            newModel.ToGlTF("test.glb");
+        }
+    }
+}
